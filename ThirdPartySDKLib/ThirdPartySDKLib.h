fbae47dfbea3ac23d0dba297f2e402859ec71e90
hao-ming <hao-ming@meyougames.com>
2013-10-13 23:58:15 +0800

    Initial Commit

---
+++ ThirdPartySDKLib/ThirdPartySDKLib.h
@@ -0,0 +1,13 @@
+//
+//  ThirdPartySDKLib.h
+//  ThirdPartySDKLib
+//
+//  Created by MeYouGames on 13-10-13.
+//  Copyright (c) 2013年 MeYouGames. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ThirdPartySDKLib : NSObject
+
+@end
---
+++ ThirdPartySDKLib/ThirdPartySDKLib.m
@@ -0,0 +1,13 @@
+//
+//  ThirdPartySDKLib.m
+//  ThirdPartySDKLib
+//
+//  Created by MeYouGames on 13-10-13.
+//  Copyright (c) 2013年 MeYouGames. All rights reserved.
+//
+
+#import "ThirdPartySDKLib.h"
+
+@implementation ThirdPartySDKLib
+
+@end
