dddca34f8c9244aa07b4555d116d6914877cced7
wang-yichun <wang-yichun@meyougames.com>
2013-10-18 10:06:51 +0800

    get my profile 完成
    Signed-off-by: wang-yichun <wang-yichun@meyougames.com>

--- ThirdPartySDKLib/Tango/TangoManager.mm
+++ ThirdPartySDKLib/Tango/TangoManager.mm
@@ -85,7 +85,7 @@ static TangoManager *_tangoManager = nil;
 
 - (void)getMyProfile:(NSObject *)prms {
     
-    NSLog(@"purchase something called");
+    NSLog(@"getMyProfile");
     NSDictionary *parameters = (NSDictionary*)prms;
     NSLog(@"Passed params are : %@", parameters);
     NSString* CPPFunctionToBeCalled = (NSString*)[parameters objectForKey:@"simple_callback"];
@@ -209,12 +209,6 @@ static TangoManager *_tangoManager = nil;
     });
 }
 
-//- (void)getMyProfile_profileID:(NSString *)profileID
-//                       uiImage:(UIImage *)image
-//{
-//    
-//}
-
 - (void)SampleSelector:(NSObject *)prms
 {
     NSLog(@"purchase something called");
@@ -238,37 +232,5 @@ static TangoManager *_tangoManager = nil;
     // with the string we are passing
     [IOSNDKHelper SendMessage:CPPFunctionToBeCalled WithParameters:nil];
 }
-
-//+ (NSString*)base64forData:(NSData*)theData
-//{
-//    const uint8_t* input = (const uint8_t*)[theData bytes];
-//    NSInteger length = [theData length];
-//    
-//    static char table[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
-//    
-//    NSMutableData* data = [NSMutableData dataWithLength:((length + 2) / 3) * 4];
-//    uint8_t* output = (uint8_t*)data.mutableBytes;
-//    
-//    NSInteger i;
-//    for (i=0; i < length; i += 3) {
-//        NSInteger value = 0;
-//        NSInteger j;
-//        for (j = i; j < (i + 3); j++) {
-//            value <<= 8;
-//            
-//            if (j < length) {
-//                value |= (0xFF & input[j]);
-//            }
-//        }
-//        
-//        NSInteger theIndex = (i / 3) * 4;
-//        output[theIndex + 0] =                    table[(value >> 18) & 0x3F];
-//        output[theIndex + 1] =                    table[(value >> 12) & 0x3F];
-//        output[theIndex + 2] = (i + 1) < length ? table[(value >> 6)  & 0x3F] : '=';
-//        output[theIndex + 3] = (i + 2) < length ? table[(value >> 0)  & 0x3F] : '=';
-//    }
-//    
-//    return [[[NSString alloc] initWithData:data encoding:NSASCIIStringEncoding] autorelease];
-//}
 
 @end
